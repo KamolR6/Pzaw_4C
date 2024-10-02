088391172b71ea3581e1ffb6ab0d9f3e1b7d9bcf
test <test@DOM.SCI.EDU.PL>
2024-10-02 09:25:07 +0200

    Added first ref, created first functions

--- project_react/src/Sci.js
+++ project_react/src/Sci.js
@@ -1,9 +1,23 @@
+import { useRef } from "react";
+
 function Sci() {
+    //referencja wskazujaca na dany element
+    const inputRef = useRef();
+
+    const ButtonHandler = () => {
+        console.log("ButtonHandler active")
+        console.log(inputRef.current.value);
+    }
+    const InputHandler = (event) => {
+        console.log(event.target.value);
+    }
     return (
         <div>
             <p>
                 SCI
             </p>
+            <input type="button" value="It's not a button" onClick={ButtonHandler}></input>
+            <input ref={inputRef} type="text" onChange={InputHandler}></input>
         </div>
     )
 }
