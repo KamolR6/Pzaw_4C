65e8f656b9a439d5095a50351b70792fb5fa249d
Test test <test@sci.edu.pl>
2024-10-09 08:30:52 +0200

    useEffect and funny useState

--- project_react/src/FirstComponent.js
+++ project_react/src/FirstComponent.js
@@ -1,16 +1,28 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./FirstCss.css"
 
 
 
 function FirstComponent() {
+
     const [name, setName] = useState("Kamil")
-
-    const ButtonHandler = () => {
+    const [message, setMessage] = useState("")
+    const NameSetter = () => {
         console.log("ButtonHandler active")
-        setName("Adam")
+        if (name == "Kamil") {
+            setName("Adam")
+        }else{
+            setName("Kamil")
+        }
     }
-
+    const SayNameHandler = () =>{
+        setMessage("You're godamn right.")
+    }
+    //Nasluchuje na wszystkie stany useState w komponencie, 2 arg [] = wykonuje sie raz, [name] = nasluchuje tylko na zmiane name
+    useEffect(() => {
+        console.log("Effect is called.")
+    }, [name])
+    
     const myStyle = {
         color: "red",
         fontSize: "32px"
@@ -23,7 +35,15 @@ function FirstComponent() {
             <p style={myStyle}>
                 Panie adminie proszę mi konto naprawic. {name}
             </p>
-            <input type="button" value="Change my name." onClick={ButtonHandler}></input>
+            <input type="button" value="Change my name." onClick={NameSetter}></input>
+
+            <p>
+                Say my name.
+            </p>
+            <input type="button" value={name} onClick={SayNameHandler}></input>
+            <p>
+                {message}
+            </p>
         </div>
     )
 }
