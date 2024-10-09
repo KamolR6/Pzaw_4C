63c35a2f08364392f6cf7eb70fc71834b6351f02
Test test <test@sci.edu.pl>
2024-10-09 07:48:52 +0200

    Pulled repo and it made changes to index.js and app,js

--- project_react/src/App.js
+++ project_react/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          YOOOOO
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://www.sci.edu.pl/"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn how to lose sanity.
+          Learn React
         </a>
       </header>
     </div>
