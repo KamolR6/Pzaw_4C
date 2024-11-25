7aded6383763e82ff1ba218c40b9f858a119ce73
Test test <test@sci.edu.pl>
2024-11-25 16:29:40 +0100

    deleted useless imports

--- project_react/src/index.js
+++ project_react/src/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './CssIndex.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import FirstComponent from './FirstComponent';
-import Form from './Form';
-import FirstTable from './FirstTable';
-import FirstRow from './FirstRow';
-import AxiosExample from './AxiosExample';
-import TaskFormWithBackend from './18_11_Task';
+import TaskPokedex from './TaskPokedex';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <FirstComponent /> */}
-    {/* <Form /> */}
-    {/* <FirstTable />
-    <FirstRow /> */}
-    {/* <AxiosExample></AxiosExample> */}
-    <TaskFormWithBackend></TaskFormWithBackend>
+    <TaskPokedex></TaskPokedex>
   </React.StrictMode>
 );
 
