cd713a70110d9047e226df1d0520bcec1494b882
Test test <test@sci.edu.pl>
2024-11-25 16:29:14 +0100

    Backend changes (deleted useless paths and added nessescary)

--- project_backend/index.js
+++ project_backend/index.js
@@ -1,49 +1,54 @@
 const express = require("express")
 const cors = require("cors")
+const e = require("express")
 
 
 const application = express()
 application.use(cors())
-const dataGlobal = []
 application.use(express.json())
+const dataGlobal = []
 
-application.get("/", (request, response) => {
-    response.send("<div><p> Fortnite is the best game in the universe </p> </div>")
+filePathPokedex = require("../project_backend/pokedex.json")
+filePathTypes = require("../project_backend/types.json")
+pokemonRouter = express.Router()
+pokemonRouter.use(express.json())
+const dataPokedex = []
+const dataTypes = []
+
+pokemonRouter.post("/", (request, response) => {
+
+})
+pokemonRouter.get("/", (request, response) => {
+    filePathPokedex.forEach(element => {
+        response.send(
+            element
+        )
+    })
+
+})
+pokemonRouter.get("/type", (request, response) => {
+    dataTypes.push(filePathTypes.map(el => el.english))
+    response.json(dataTypes[0]);
+
+    // filePathTypes.forEach(element => {
+    //     response.send(
+    //         element.english
+    //     )    
+    // })
+    // response.send(filePathTypes.map((el,id) => {el.english}))
 })
 
-application.get("/fortnite", (request, response) => {
-    response.status(500)
-    response.send("<div><img src='https://static.wikia.nocookie.net/fortnite/images/1/11/Bars_(Large)_-_Currency_-_Fortnite.png'></div>")
-})
-
-application.post("/abc", (request, response) => {
-    const data = request.body
-    console.log(data)
-    dataGlobal.push(data)
-    response.send()
-})
-application.get("/cba", (request, response) => {
-    response.send(dataGlobal)
-})
-// application.get("/cba", (express.request, express.response) => {
+// application.post("/abc", (request, response) => {
+//     const data = request.body
+//     console.log(data)
+//     dataGlobal.push(data)
+//     response.send()
+// })
+// application.get("/cba", (request, response) => {
 //     response.send(dataGlobal)
-//})
-// sci/4c/abc
-const sci_router = express.Router()
-sci_router.get("/", (request, response) => {
-    response.send("ruter orange");
-})
-sci_router.get("/json", (request, response) => {
-    const data = {
-        ch1s7: "winterfest",
-        ch2s2: "Sentino - Midas",
-        ch2s4: "Avendzers"
-    }
-    response.json(data)
-})
+// })
 
-application.use("/sci", sci_router)
-
+application.use("/pokedex", pokemonRouter)
 application.listen(8000, () => {
     console.log("Yo skurkowancu, serwer's up!")
 })
