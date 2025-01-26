49a850ee84e1378eb7cac7780092a9f59f9804c1
KamolR6 <kamilbar02@gmail.com>
2025-01-26 18:29:51 +0100

    swapped index to work with query tanstack

--- project_react/src/index.js
+++ project_react/src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './CssIndex.css';
-import App from './App';
-import TaskPokedex from './TaskPokedex';
-import TaskTanstackForm from './TaskTanstackForm'
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import PhotoGallery from "./gallery"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <TaskTanstackForm></TaskTanstackForm>
-  </React.StrictMode>
+// Create the query client instance
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={queryClient}>
+    <PhotoGallery></PhotoGallery>
+  </QueryClientProvider>,
+  document.getElementById("root")
 );
 
-// reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './CssIndex.css';
+// import App from './App';
+// import TaskPokedex from './TaskPokedex';
+// import TaskTanstackForm from './TaskTanstackForm'
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <TaskTanstackForm></TaskTanstackForm>
+//   </React.StrictMode>
+// );
+
+// // reportWebVitals();
